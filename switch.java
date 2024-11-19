46c99fdedee6e752d15457ac1eee885c07634a4d
Hema334 <112267828+Hema334@users.noreply.github.com>
2024-11-19 23:13:45 +0530

    Add files via upload

---
+++ switch.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	     int val=65;
+	     switch(val)
+	     {
+	         case'A':
+	                 System.out.println("This is the first letter");
+	                 break;
+	           case 'B':
+	               System.out.println("this is the second letter");
+	               break;
+	               default:
+	               System.out.println("this dos not match");
+	     }
+	}
+}
